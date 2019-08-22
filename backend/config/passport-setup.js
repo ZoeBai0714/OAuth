d046dd43660c0f836f2f0c3fa6cab0365156cc6a
github username <s.xue.bai@gmail.com>
2019-08-22 12:02:18 -0500

    passport setup

---
+++ backend/config/passport-setup.js
@@ -0,0 +1,10 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20");
+
+//takes two args, one is your strategy, another one is a callback function
+passport.use(new GoogleStrategy({
+    //options for strategy
+}), ()=>{
+    
+ 
+});
