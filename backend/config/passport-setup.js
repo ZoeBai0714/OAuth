8c56fa81b4bec551c0689ccd673ff2f97e7f1e4b
github username <s.xue.bai@gmail.com>
2019-08-22 14:49:34 -0500

    created clientID and clientSecret for google+, securely stored

--- backend/config/passport-setup.js
+++ backend/config/passport-setup.js
@@ -1,9 +1,12 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20");
+const keys = require("./keys");
 
 //takes two args, one is your strategy, another one is a callback function
 passport.use(new GoogleStrategy({
-    //options for strategy
+    //options for strategy. Created on google console developer
+    clientID:keys.google.clientId,
+    clientSecret:keys.google.clientSecret
 }), ()=>{
     
  
